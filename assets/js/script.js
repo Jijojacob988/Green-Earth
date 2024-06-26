df8098f068423a358df5616a7f4c3c77cd4398a4
Jijo Jacob <jijojacob988@gmail.com>
2024-06-26 13:40:46 +0530

    navbar created & designed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,25 @@ window.addEventListener("load", ()=>{
     preloader.classList.add("remove");
     
 });
+
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");  // Corrected this line
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
